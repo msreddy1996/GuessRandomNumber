b2c2426dcec556455a2e4865e4bed0c1483960e5
msreddy1996 <smallano@oldwestbury.edu>
2022-07-07 23:24:07 -0400

    guess-rndom-number

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessRandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random r = new Random();
+            int winNum = r.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess Number between 1 and 100");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if(i>winNum)
+                {
+                    Console.WriteLine("Too high , Guess Lower");
+                }
+                else if (i< winNum)
+                {
+                    Console.WriteLine("Too Low , Guess Higher");
+                }
+                else if(i==winNum)
+                {
+                    Console.WriteLine("Congrats you matched the number");
+                    win = true;
+                }
+
+                Console.WriteLine();
+            } while (win == false);
+            Console.WriteLine("Thank you for playing the game");
+            Console.Write("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
